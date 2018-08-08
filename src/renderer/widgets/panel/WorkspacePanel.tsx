b7681f0fcb2cdf34ae9b7202ece73423bbe1ed85
Tobias Ulrich <flamenco.bluegrass@gmail.com>
2018-08-07 23:39:26 -0300

    Fixed dock panel style

--- src/renderer/widgets/panel/WorkspacePanel.tsx
+++ src/renderer/widgets/panel/WorkspacePanel.tsx
@@ -1,7 +1,7 @@
 import { DockPanel, Widget } from "@phosphor/widgets";
 import * as React from "react";
 //import { createPortal } from "react-dom";
-import './dock-panel.css';
+import './style/index.css';
 
 interface IWidgetInfo {
     component: JSX.Element;
@@ -28,26 +28,28 @@ interface IDockProps {
  */
 function createContent(title: string): Widget {
     var widget = new Widget();
+    widget.setFlag(Widget.Flag.DisallowLayout);
     widget.addClass('content');
     widget.addClass(title.toLowerCase());
-  
+
     widget.title.label = title;
     widget.title.closable = true;
-  
+    widget.title.caption = `Long description for: ${name}`;
+
     return widget;
-  }
-  
+}
+
 
 
 export default class WorkspacePanel extends React.PureComponent<IDockProps, IDockState>  {
 
     elem: HTMLElement;
     dock: DockPanel;
-    width:number;
-    height:number;
+    width: number;
+    height: number;
 
     componentWillMount() {
-        
+
         this.dock = new DockPanel();
 
         let children = [];
@@ -59,14 +61,28 @@ export default class WorkspacePanel extends React.PureComponent<IDockProps, IDoc
 
         let widgetInfos = [];
         for (let component of children) {
-            let node = document.createElement("div");
+            //let node = document.createElement("div");
             //let widget = new WrapperWidget("Widget Name", node);
             this.dock.addWidget(component);
-            widgetInfos.push({ node, component });
+            //widgetInfos.push({ node, component });
         }
         this.setState({ ...this.state, widgetInfos });
         this.dock.id = 'main';
-        
+        //let parent = this.dock.handleEvent.bind(this.dock);
+
+        // const wrapper = (event: any) => {
+        //     if (event.type === 'p-dragover') {
+        //         event.dropAction = event.proposedAction;
+        //         event.preventDefault();
+        //         event.stopPropagation();
+        //     } else {
+        //         parent(event);
+        //     }
+        // }
+
+        //this.dock.handleEvent = wrapper.bind(this.dock);
+
+
     }
 
     private resize() {
@@ -86,9 +102,9 @@ export default class WorkspacePanel extends React.PureComponent<IDockProps, IDoc
             this.width = w;
             this.height = h;
             this.elem.style.width = (w - 2).toString() + 'px';
-            this.elem.style.height=  (h - 64).toString() + 'px';
+            this.elem.style.height = (h - 64).toString() + 'px';
             this.elem.style.minWidth = (w - 2).toString() + 'px';
-            this.elem.style.minHeight =  (h - 64).toString() + 'px';
+            this.elem.style.minHeight = (h - 64).toString() + 'px';
             // this.elem.style.top = '1px';
             // this.elem.style.left = '1px';
             // this.elem.style.right = '1px';
@@ -102,16 +118,16 @@ export default class WorkspacePanel extends React.PureComponent<IDockProps, IDoc
     componentDidMount() {
         this.elem = document.getElementById('dock-panel');
         this.resize();
-   
+
         DockPanel.attach(this.dock, this.elem);
-        
+
         //Widget.attach(this.dock, this.elem);
 
         window.onresize = (event) => {
             this.resize();
             this.dock.update();
             //this.dock.fit();
-             
+
         };
     }
 
